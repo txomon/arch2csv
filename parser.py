85bf7ed08a9bf42a5765f16c296ee68320d0e915
Javier Domingo <javierdo1@gmail.com>
2012-12-26 19:45:37 +0100

    He creado el esqueleto del script/módulo, haciendo que sea posible añadirlo como módulo para más adelante. Además, estoy utilizando dependencias con argparse, una librería muy buena y además del sistema para parsear argumentos. Ha sido añadida en python3.2

--- parser.py
+++ parser.py
@@ -16,37 +16,91 @@
 
 class LogFile:
 	"""
-	It is a log file, with all the rows accessible in the right order
+	It represents a log file, with all the rows accessible in 
+	the right order
 	"""
 
-	def __init__(self, logfilename):
+	
+	def __init__(self, logfile, sep=": "):
+		""" (file[, sep=": "])
+		Receives the logfile it has to parse and extract the information
+		from using sep as the separator between attribute and value
 		"""
-		Receives the file name it has to parse and extract the attributes from
-		"""
+		self.dictionary = {}
+		
 
 	def get_values(self, attribute):
-		"""
-		It returns a list of the values of the attribute in the file
+		""" (str) -> list of str
+		It returns a list of the values of the attribute in 
+		the file
 		"""
 
 	def get_attributes(self):
-		"""
-		It return a list of the attributes in the log file
+		""" () -> list of str
+		It returns the list of attributes in the log file
 		"""
 
 
 class OutFile:
 	"""
-	It is the output file that has all the selected attributes with their values
+	It is the output file that has all the selected attributes with their
+values
 	"""
-	def __init__(self, outfilename):
+	def __init__(self, outfile):
 		"""
 		Receives the name of the output file it will use
 		"""
 
-	def print(self, attributes, values):
+	def write_attributes(self, attributes):
 		"""
 		Prints the attributes with its values in the file
 		"""
 
-class Parser:
+class GraphicFile:
+	"""
+	It is meant to be a Graphics file that represents the data
+	"""
+	pass
+
+
+class XMLFile:
+	"""
+	It is the xml input file that is meant to describe what 
+	the program has to do
+	"""
+	def __init__(self, xml_file):
+		"""
+		Receives the xml_file and parses it, creating its internal
+		data structure
+		"""
+
+# If this script is used as an script
+if __name__ == "__main__":
+
+	###
+	## We first start parsing arguments
+	import argparse
+	
+	# First, we describe the program
+	arg_parser = argparse.ArgumentParser(
+		description="A script that parses log files and outputs them as csv files",
+		epilog="Bugs should be sent to javierdo1<at>gmail<dot>com",
+		)
+	
+	# Then we start adding the arguments it supports
+
+	# The xml config file is totally needed, it is the first argument to be put
+	arg_parser.add_argument("xml_config", nargs=1,
+				type=argparse.FileType("r"),
+				help="The XML config file you want to use")
+
+	# The log files to be parsed should be passed
+	arg_parser.add_argument("log_file", nargs="+", action="append",
+				type=argparse.FileType("r"),
+				help="The log file to be parsed")
+	
+	arguments = arg_parser.parse_args()
+	## Ends argument parsing
+	###
+	
+	
